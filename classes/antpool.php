e594e6f0131df90746864270f7e3fa719b63eaa3
Elompenta <lutz@baebeca.de>
2016-01-02 20:52:33 +0100

    fix comments

--- classes/antpool.php
+++ classes/antpool.php
@@ -1,15 +1,13 @@
 <?php
 /*!
  * @author		Sebastian Lutz
- * @copyright	Baebeca Solutions - Lutz
+ * @copyright 	Baebeca Solutions - Lutz
  * @email		lutz@baebeca.de
  * @pgp			0x5AD0240C
  * @link		https://www.baebeca.de
  * @link-github	https://github.com/Elompenta/antpool-php-api
- * @customer	-
+ * @customer 	-
  * @project		antpool-php-api
- * @version		$Revision: 2178 $
- * @date		$Date: 2015-12-05 12:35:40 +0100 (Sa, 05 Dez 2015) $
  * @license		GNU GENERAL PUBLIC LICENSE Version 2
  **/
 
